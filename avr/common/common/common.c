9b515af6391b134f3582fac9ea49a76043e33793
Matilda Dahlström <matilda.dahlstrom@gmail.com>
2016-11-03 17:16:03 +0100

    Added abillity for the robot to move. The wheel pairs can be controlled independently of each other

--- avr/common/common/common.c
+++ avr/common/common/common.c
@@ -9,8 +9,50 @@
 #include <avr/io.h>
 #include "debug.h"
 
+void initialize_PWM();
+void set_robot_speed(int speed);
+
 int main(void)
 {
+	
 	initialize_uart();
-	printf("Device booted.\n");
+	initialize_PWM();
+	set_robot_speed(2);
+		
+	int counter = 0;
+	while (1) {
+
+		
+		counter = TCNT1;
+		if(counter < 0) {
+			printf("counter negative");
+		}
+		else{
+			printf("Counter value: %d\n", TCNT1);
+		}
+	}
 }
+
+void set_robot_speed(int speed) {
+	OCR1A = speed;	// Set speed left wheels
+	OCR1B = speed;	// Set speed right wheels
+			
+}
+
+void initialize_PWM() {
+	
+	DDRA = 0xFF;	// Set Data Direction on PortA
+	DDRD = 0xFF;	// Set Data Direction on PortD
+	TCNT1 = 0;		// Reset Timer1 counter
+	
+	TCCR1A = 0;		// Clear Timer1 settings
+	TCCR1B = 0;		// Clear Timer1 settings
+		
+	PORTA = 0xF1;	// Set direction of wheels (Pin 40 for left wheels, pin 38 for right wheels)
+		
+	TCCR1B|= (1<<WGM12)|(1<<CS12)|(1<<CS10)|(1<<WGM13); // Prescaler 1024, Timer1 settings
+	TCCR1A|= (1<<COM1A1)|(1<<WGM11)|(1<<COM1B1);		// Timer1 settings
+	ICR1 = 16;											// Set TOP	
+	
+}
+
