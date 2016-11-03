8529fedd83189b2bf6f95475e31005b6e783c751
Matildha <tildhanor@gmail.com>
2016-11-03 09:18:17 +0100

    Implemented functionality to read from both IR sensors

--- avr/sensorenhet/sensorenhet/sensorenhet.c
+++ avr/sensorenhet/sensorenhet/sensorenhet.c
@@ -7,29 +7,44 @@
 
 
 #include <avr/io.h>
-#include "debug.h";
+#include <stdbool.h>
+#include "debug.h"
 
+uint16_t adc_value;					//Variable used to store the value read from the ADC
+void adc_init(void);				//Function to initialize/configure the ADC
+uint16_t read_adc(uint8_t channel);	//Function to read an arbitrary analogic channel/pin
+
+void adc_init(void) {
+	 ADCSRA |= ((1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0));  //FEL MATTE 16Mhz/128 = 125Khz the ADC reference clock
+	 ADMUX |= (1<<REFS0);							//Voltage reference, koppla 3.3v till AREF
+	 ADCSRA |= (1<<ADEN);							//Turn on ADC
+	 ADCSRA |= (1<<ADSC);							//Do an initial conversion because this one is the slowest and to ensure that everything is up and running
+ }
+ 
+ //BUSY WAITAR. JÄTTEDÅLIGT.
+ uint16_t read_adc(uint8_t channel){
+	 ADMUX &= 0xF0;             //Clear the older channel that was read
+	 ADMUX |= channel;          //Defines the new ADC channel to be read
+	 ADCSRA |= (1<<ADSC);       //Starts a new conversion
+	 while(ADCSRA & (1<<ADSC)); //Wait until the conversion is done
+	 return ADCW;			    //Returns the ADC value of the chosen channel
+ }
+ 
 int main(void)
 {
 	initialize_uart();
+	adc_init();
 	
 	// Output
 	DDRA = 0xFF;
 	
-	ADCSRA |= ((1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0));    //Prescaler at 128 (when used with 16MHz clock, we get 125Khz)
-	
-	//ADC0 input
-	ADMUX = 0x40;
-	
-	ADCSRB &= ~((1<<ADTS2)|(1<<ADTS1)|(1<<ADTS0));    //ADC in free-running mode
-	ADCSRA |= (1<<ADATE);                //Signal source, in this case is the free-running
-	ADCSRA |= (1<<ADEN);                //Power up the ADC
-	ADCSRA |= (1<<ADSC);                //Start converting
-    
-	int adc_value;
+	uint16_t adc_value;
 	while(1)
     {
-		adc_value = ADCW;  
-		printf("Value: %d\n", adc_value);
+	    adc_value = read_adc(MUX0);
+		printf("Value ADC0: %d\n", adc_value);
+	
+		adc_value = read_adc(MUX1);
+		printf("Value ADC1: %d\n", adc_value);
 	}
 }
