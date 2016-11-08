7c0e88d7892889bd30acd83cf6e9ec1c95f40621
becczor <rebecca.lindblom@gmail.com>
2016-11-08 15:12:18 +0100

    started configuring automatic control

--- pi/main.py
+++ pi/main.py
@@ -10,16 +10,31 @@ last_request = datetime.now()
 request_period = timedelta(milliseconds=1)
 busy = False
 
+des_dist = 100 # Desired distance to wall
+
+old_e = 0
+old_t = datetime.now()
 
 def sensor_data_received(msg):
-    global busy
+    global busy, old_e, old_t
     busy = False
+    t = datetime.now()
 
     ir_left_mm = msg[1]
     ir_right_mm = msg[2]
 
     print('ir_left_mm: ' + str(ir_left_mm))
     print('ir_right_mm: ' + str(ir_right_mm))
+
+    e = des_dist - ir_left_mm # reglerfelet
+
+    # **** P-reglering *********
+    u = Kp * e # styrsignal
+
+    # ****** PD-reglering *********
+    u = Kp * e + Kd / (t - old_t) * (e - old_e)
+    old_e = e
+    old_t = t
 
 
 subscribe_to_cmd(CMD_RETURN_SENSOR_DATA, sensor_data_received)
