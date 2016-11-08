7563e9eb3ce739cb69734c862b10314ca58d01e7
becczor <rebecca.lindblom@gmail.com>
2016-11-08 17:52:58 +0100

    code for automation control

--- pi/main.py
+++ pi/main.py
@@ -14,28 +14,40 @@ last_request = datetime.now()
 request_period = timedelta(milliseconds=1)
 busy = False
 
-des_dist = 100 # Desired distance to wall
+DESIRED_DIST = 100 # Desired distance to wall
 
 old_e = 0
 old_t = datetime.now()
+Kp = 1
+
+curr_speed_l = 0
+curr_speed_r = 0
 
 def sensor_data_received(ir_left_mm, ir_right_mm):
-    global busy, old_e, old_t
+    global busy
     busy = False
     t = datetime.now()
 
     print('ir_left_mm: ' + str(ir_left_mm))
     print('ir_right_mm: ' + str(ir_right_mm))
+    
 
-    e = des_dist - ir_left_mm # reglerfelet
+# Reglerteknik
+def auto_ctrl(ir_right_mm):
+    old_e, old_t
+    e = des_dist - ir_right_mm # reglerfelet
 
     # **** P-reglering *********
     u = Kp * e # styrsignal
 
     # ****** PD-reglering *********
-    u = Kp * e + Kd / (t - old_t) * (e - old_e)
+    """u = Kp * e + Kd / (t - old_t) * (e - old_e)
     old_e = e
-    old_t = t
+    old_t = t"""
+
+    curr_speed_r += u
+    set_right_motor_speed(bus, curr_speed_r)
+
 
 
 def handle_abort(signum, frame):
@@ -47,6 +59,10 @@ def handle_abort(signum, frame):
 # Setup
 subscribe_to_cmd(CMD_RETURN_SENSOR_DATA, sensor_data_received)
 signal.signal(signal.SIGINT, handle_abort)
+
+curr_speed_l = 3
+curr_speed_r = 3
+set_motor_speed(bus, curr_speed_r)
 
 while True:
     read_messages(bus)
