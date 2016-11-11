de1540e8de5c1d82dded91eb86995a1736d34717
antondalgren <antda685@student.liu.se>
2016-11-11 12:59:48 +0100

    snabbare runt

--- pi/main.py
+++ pi/main.py
@@ -22,8 +22,8 @@ old_e = 0
 old_t = datetime.datetime.now()
 Kp = 0.1
 
-TURN_SPEED = 20
-TURN_TIME = 1000
+TURN_SPEED = 40
+TURN_TIME = 2000
 turn_start_time = 0
 curr_speed_l = 0
 curr_speed_r = 0
