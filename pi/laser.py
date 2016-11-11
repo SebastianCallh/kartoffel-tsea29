c1bd89404e4c5ac9bbe38331d5fc902670e45a8b
antondalgren <antda685@student.liu.se>
2016-11-11 12:27:38 +0100

    changed laser read address

--- pi/laser.py
+++ pi/laser.py
@@ -4,8 +4,8 @@ def init_laser_data(bus):
 
 def read_laser_data(bus):
     try:
-        hi = bus.read_byte_data(0x62, 0x14)
-        lo = bus.read_byte_data(0x62, 0x15)
+        hi = bus.read_byte_data(0x62, 0x0f)
+        lo = bus.read_byte_data(0x62, 0x10)
         data = (hi << 8) | lo
         return data
     except:
