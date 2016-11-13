b9e87d50cbc11935d70487d6f2ddbe7f644fac06
Sebastian Callh <sebca553@ixtab.edu.isy.liu.se>
2016-11-13 16:57:32 +0100

    commiting comments mostly. Still broken

--- pi/lcd.py
+++ pi/lcd.py
@@ -1,14 +1,15 @@
 import RPi.GPIO as GPIO
-
+from time import sleep
 
 #Times in ns
 E_CYCLE = 500
 E_RISE_FALL = 20
 E_PULSE_WIDTH = 230
+
 DATA = [0 for x in range(0, 7)]
 
 '''
-Anslutningar till GPIO-portar
+Anslutningar till GPIO-portar. Alla portar är i board-numbering
 GPIO 	29, 31, 32, 33, 35, 36, 37, 38
 GROUND 	30
 5V/VCC 	02
@@ -16,28 +17,58 @@ R_W 	22
 E		40
 '''
 
+#29 is MSB, 38 LSB
 pins = [29, 31, 32, 33, 35, 36, 37, 38]
+RS = 18
 R_W = 22
 E = 40
 
+FUNCTION_SET = [0, 0, 1, 1, 1, 1, 0, 0]
+DISPLAY_ONOFF_CONTROL = [0, 0, 0, 0, 1, 1, 1, 1] #display, cursor, blink on 
+DISPLAY_CLEAR = [0, 0, 0, 0, 0, 0, 0, 1]
+ENTRY_MODE_SET = [0, 0, 0, 0, 0, 1, 1, 0] #increment mode, entire shift off
+DISPLAY_ON = [0, 0, 0, 0, 1, 1, 0, 0]	
+	
 class LCD:
 	def init(self):	
 		#Use board numbering
 		GPIO.setmode(GPIO.BOARD)
 		
+		#Configure output pins
 		GPIO.setup(R_W, GPIO.OUT)
 		GPIO.setup(E, GPIO.OUT)
-		print('R_W, E output')
-		
 		for p in pins:
 			GPIO.setup(p, GPIO.OUT)
-			print('pin ' + str(p) + ' output')
-			
+
+		GPIO.output(RS, 0)		#Select instruction register
+		GPIO.output(E, 0) 		#Make sure E is initially low
+		GPIO.output(R_W, 0) 	#Make sure R_W is initially low
+		
+		#Power up sequence
+		sleep(40)				#Make sure at least 30 ms has passed since power on
+		self.send(FUNCTION_SET)
+		sleep(2)				#Make super sure that 39 us mas passed
+		self.send(DISPLAY_ONOFF_CONTROL)
+		sleep(2)				#Make super sure that 39 us mas passed again
+		self.send(DISPLAY_CLEAR)
+		sleep(4)				#Make sure that 1.53 ms has passed
+		self.send(ENTRY_MODE_SET)
+		#End of power up sequence. Display is off
+		
+		#Turn on display
+		self.send(DISPLAY_ON)
+		sleep(80)				#Wait a lot 
+		
+		
 			
 	def send(self, data):
+		GPIO.output(E, 0) 		#Make sure E is initially low
+		GPIO.output(R_W, 0) 	#Make sure R_W is initially low
+		GPIO.output(RS, 0)		
+		
 		for d, p in zip(data, pins):
 			GPIO.output(p, d)
-			print('setting ' + str(p) + ' to ' + str(d))
 			
 	def cleanup(self):
 		GPIO.cleanup()
+		
