ede52bddc5c7d3e652ae2d69e2e42bd85ab6ba86
Sebastian Callh <sebca553@ixtab.edu.isy.liu.se>
2016-11-15 10:50:17 +0100

    more indentation

--- pi/main.py
+++ pi/main.py
@@ -9,7 +9,7 @@ from navigator import Navigator
 
 from messages import read_messages, subscribe_to_cmd
 from outbound import request_sensor_data, CMD_RETURN_SENSOR_DATA, \
-    set_motor_speed, set_right_motor_speed, set_left_motor_speed
+set_motor_speed, set_right_motor_speed, set_left_motor_speed
 
 bus = Bus()
 navigator = Navigator()
@@ -23,32 +23,31 @@ def sensor_data_received(ir_left_mm, ir_right_mm):
 	global busy
 	busy = False
 	navigate.sensor_data_received(ir_left_mm, ir_right_mm)
-	
-def handle_abort(signum, frame):
-    # Stop motors to avoid robot running amok
-    set_motor_speed(bus, 0)
 
-    sys.exit(0)
+def handle_abort(signum, frame):
+	# Stop motors to avoid robot running amok
+	set_motor_speed(bus, 0)
+
+	sys.exit(0)
 
 def handle_bus(bus):
 	if not busy and datetime.datetime.now() - last_request > request_period:
-		busy = True
-		last_request = datetime.datetime.now()
+	busy = True
+	last_request = datetime.datetime.now()
 
-		request_sensor_data(bus)
+	request_sensor_data(bus)
 
 # Setup
 signal.signal(signal.SIGINT, handle_abort)
 subscribe_to_cmd(CMD_RETURN_SENSOR_DATA, sensor_data_received)
 
 try:
-	
-    while True:
+	while True:
 		read_messages(bus)
 		handle_bus(bus)
 		navigator.navigate()
-		
-		
+
+
 except:
-    traceback.print_exc()
-    set_motor_speed(bus, 0)
+	traceback.print_exc()
+	set_motor_speed(bus, 0)
