8e1c57dd08aec72f584ab17796256310dbe5a1ab
Sebastian Callh <sebca553@ixtab.edu.isy.liu.se>
2016-11-15 11:24:29 +0100

    self in driver methods

--- pi/driver.py
+++ pi/driver.py
@@ -3,12 +3,11 @@ from outbound import set_motor_speed
 	
 ###### METHODS FOR CONTROLLING THE WHEELS #######	
 
+TURN_SPEED = 45
+TURN_TIME = 900
 
 class Driver:
 
-	TURN_SPEED = 45
-	TURN_TIME = 900
-	
 	def __init__(self, bus):
 		self.bus = bus
 		self.drive_stop_time = 0
@@ -24,10 +23,10 @@ class Driver:
 		set_motor_speed(self.bus, left_speed, right_speed)
 			
 	def turn_left(self):
-		drive(-TURN_SPEED, TURN_SPEED, TURN_TIME)
+		self.drive(-TURN_SPEED, TURN_SPEED, TURN_TIME)
 
 	def turn_right(self):
-		drive(TURN_SPEED, -TURN_SPEED, TURN_TIME)
+		self.drive(TURN_SPEED, -TURN_SPEED, TURN_TIME)
 		
 	def prepare_for_turn(self):
-		drive(40, 40, 600)
+		self.drive(40, 40, 600)
