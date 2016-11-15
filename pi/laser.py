cc6a3ad75b6d2a9e7e40bb9a7d28487b9b657c51
Matilda Dahlström <matilda.dahlstrom@gmail.com>
2016-11-15 20:02:10 +0100

    commented out bork laser

--- pi/laser.py
+++ pi/laser.py
@@ -6,14 +6,14 @@ class Laser:
     @staticmethod
     def initialize():
 		#Was bus.write_byte_data, but that method was renamed/removed
-        EventBus.bus.write_packet_data(LASER_ADDR, 0x11, 0xff) #sets laser to read forever
-        EventBus.bus.write_packet_data(LASER_ADDR, 0x00, 0x04) #sets laser to start reading
+        EventBus.bus._write_byte_data(LASER_ADDR, 0x11, 0xff) #sets laser to read forever
+        EventBus.bus._write_byte_data(LASER_ADDR, 0x00, 0x04) #sets laser to start reading
 
     @staticmethod
     def read_data():
         try:
-            hi = EventBus.bus.read_packet_data(LASER_ADDR, 0x0f)
-            lo = EventBus.bus.read_packet_data(LASER_ADDR, 0x10)
+            hi = EventBus.bus._read_packet_data(LASER_ADDR, 0x0f)
+            lo = EventBus.bus._read_packet_data(LASER_ADDR, 0x10)
             data = (hi << 8) | lo
             return data * 10
         except:
