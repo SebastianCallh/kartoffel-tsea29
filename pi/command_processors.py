eaa41055f0898f7a977033a24ae0e57cd8448450
Sebastian Callh <sebca553@ixtab.edu.isy.liu.se>
2016-11-16 18:50:03 +0100

    more pep8:ing

--- pi/command_processors.py
+++ pi/command_processors.py
@@ -9,20 +9,21 @@ COMMAND_PROCESSORS = {}
 
 
 def process_arguments(message_id, arguments):
-	if message_id in COMMAND_PROCESSORS:
-		return COMMAND_PROCESSORS[message_id](*arguments)
+    if message_id in COMMAND_PROCESSORS:
+        return COMMAND_PROCESSORS[message_id](*arguments)
 
-	return arguments
+    return arguments
+
 
 # Sensor data contains some arguments stored in 16-bit two's complement which
 # must be parsed into it's corresponding python values.
 def process_sensor_data(left_ir_mm_hi, left_ir_mm_lo, right_ir_mm_hi, right_ir_mm_lo, *args):
-	left_ir_mm = twos_comp((left_ir_mm_hi << 8) | left_ir_mm_lo, 16)
-	right_ir_mm = twos_comp((right_ir_mm_hi << 8) | right_ir_mm_lo, 16)
+    left_ir_mm = twos_comp((left_ir_mm_hi << 8) | left_ir_mm_lo, 16)
+    right_ir_mm = twos_comp((right_ir_mm_hi << 8) | right_ir_mm_lo, 16)
 
-	print('ir_left: ' + str(left_ir_mm) + ' ir_right: ' + str(right_ir_mm) + ' args: ' + str(args))
-	print('concat: ' + str([left_ir_mm, right_ir_mm] + list(args)))
-	return [left_ir_mm, right_ir_mm] + list(args)
+    print('ir_left: ' + str(left_ir_mm) + ' ir_right: ' + str(right_ir_mm) + ' args: ' + str(args))
+    print('concat: ' + str([left_ir_mm, right_ir_mm] + list(args)))
+    return [left_ir_mm, right_ir_mm] + list(args)
 
 
 COMMAND_PROCESSORS[CMD_RETURN_SENSOR_DATA] = process_sensor_data
