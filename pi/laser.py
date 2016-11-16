c1dc9763299ef771f90db05db6954a0ec44e0e9f
Sebastian Callh <sebca553@ixtab.edu.isy.liu.se>
2016-11-16 21:20:43 +0100

    laser from laser branch

--- pi/laser.py
+++ pi/laser.py
@@ -1,10 +1,11 @@
 from bus import LASER_ADDR
 from eventbus import EventBus
-from time import sleep
+
 
 class Laser:
     @staticmethod
     def initialize():
+		#Was bus.write_byte_data, but that method was renamed/removed
         EventBus.bus.bus.write_byte_data(LASER_ADDR, 0x00, 0x00) #Resets FPGA registers
         sleep(1)
         EventBus.bus.bus.write_byte_data(LASER_ADDR, 0x11, 0xff) #sets laser to read forever
@@ -16,6 +17,9 @@ class Laser:
             hi = EventBus.bus.bus.read_byte_data(LASER_ADDR, 0x0f)
             lo = EventBus.bus.bus.read_byte_data(LASER_ADDR, 0x10)
             data = (hi << 8) | lo
-            return data * 10
+            if hi & 0x80 == 128:
+                return -1
+            else:
+                return data * 10
         except:
             return -1
