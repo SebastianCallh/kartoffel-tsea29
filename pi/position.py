2be04f6a9b7d070938dfd4fca60235c17ed0da20
antondalgren <antda685@student.liu.se>
2016-11-17 08:54:33 +0100

    Reverted changes

--- pi/position.py
+++ pi/position.py
@@ -40,8 +40,8 @@ class Position:
 
     def on_turning_started(self):
         self.state = STATE_WAITING
-        thread = Thread(target=self.save_current_section)
-        #thread.start()
+        thread = Thread(target=self.save_current_section())
+        thread.start()
         #self.save_current_section()
 
     def on_turning_finished(self, is_right_turn):
