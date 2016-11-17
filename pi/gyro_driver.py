a38609ed62daa4882cc10ab6874bb0731d776551
Sebastian Callh <sebca553@ixtab.edu.isy.liu.se>
2016-11-17 11:26:50 +0100

    advanced driver yo

--- pi/gyro_driver.py
+++ pi/gyro_driver.py
@@ -1,11 +1,25 @@
-from gyro import Gyro
 from time import sleep
+from datetime import datetime, timedelta
+
+from gyro import Gyro
+from driver import Driver
 
 gyro = Gyro()
 gyro.initialize()
 
+time_delta = datetime.now().total_seconds()
+degrees_total = 0
+
+GYRO_LOWER_LIMIT = 10
 
 while(True):
-    sleep(0.2)
     data = gyro.read_data()
-    print(data)
+
+    if abs(data) <= GYRO_LOWER_LIMIT:
+        continue
+        
+    time_delta = time_delta - datetime.now().total_seconds()
+    delta_degrees =  data * time_delta
+    degrees_total += delta_degrees
+    print(degrees_total)
+        
