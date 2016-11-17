96073587c1497c1545135f28678fe0f2b47b55e8
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-11-17 07:49:42 +0100

    Connected navigation events to positioning system

--- pi/navigator.py
+++ pi/navigator.py
@@ -3,7 +3,11 @@ State machine for navigational states
 """
 
 from autocontroller import AutoController
+from eventbus import EventBus
+from protocol import CMD_TURN_STARTED, CMD_TURN_FINISHED
 
+TURN_DIRECTION_RIGHT = True
+TURN_DIRECTION_LEFT = False
 
 
 
@@ -50,12 +54,12 @@ class auto_control(State):
         if self.is_at_right_turn(data):
             data['driver'].outer_turn_right()
             print('outer turn right')
-            return turn()
+            return turn(TURN_DIRECTION_RIGHT)
         
         if self.is_at_left_turn(data):
             data['driver'].outer_turn_left()
             print('outer turn left')
-            return turn()
+            return turn(TURN_DIRECTION_LEFT)
         
         laser_data = data['laser'].read_data()
         print('Left diff: ' + str(left_diff) + ' right diff ' + str(right_diff))
@@ -66,11 +70,11 @@ class auto_control(State):
             if data['side'] == Navigator.LEFT_SIDE:
                 data['driver'].inner_turn_right()
                 print('inner turn right')
-                return turn()
+                return turn(TURN_DIRECTION_RIGHT)
             if data['side'] == Navigator.RIGHT_SIDE:
                 data['driver'].inner_turn_left()
                 print('inner turn left')
-                return turn()
+                return turn(TURN_DIRECTION_LEFT)
                 
         return auto_control()
 
@@ -85,6 +89,9 @@ class warmup(State):
             return warmup()
 
 class turn(State):
+    def __init__(self, is_right_turn):
+        self.is_right_turn = is_right_turn
+
     def sensor_data_received(self, data, new_ir_left, new_ir_right):
         return #Do nothing. Only auto control uses it
 
@@ -93,7 +100,7 @@ class turn(State):
             print('changing to auto control')
             return auto_control()
         else:
-            return turn()
+            return turn(self.is_right_turn)
 
 ###### NAVIGATOR CLASS #######
 class Navigator:
@@ -127,4 +134,14 @@ class Navigator:
     #Runs the state. The states run method returns the next state
     def navigate(self):
         next_state = self.state.run(self.data)
+
+        curr_type = type(self.state)
+        next_type = type(next_state)
+
+        if curr_type is not turn and next_type is turn:
+            EventBus.notify(CMD_TURN_STARTED)
+
+        if curr_type is turn and next_type is not turn:
+            EventBus.notify(CMD_TURN_FINISHED, self.state.is_right_turn)
+
         self.state = next_state
