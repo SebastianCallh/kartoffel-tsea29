58768c80d82dc1c11d8936664c3b082f991cb597
antondalgren <antda685@student.liu.se>
2016-11-17 15:25:43 +0100

    Adding  some prints to idle

--- pi/driver.py
+++ pi/driver.py
@@ -21,15 +21,19 @@ class Driver:
 
     def idle(self):
         if self.task == None:
+            print("None")
             return True
 
         if not self.task.done():
+            print("Task Done")
             return False
         elif self.tasks:
+            print("Popping taaaaasks")
             self.task = self.tasks.pop()
             self.task.start()
             return False
         else:
+            print("STANNA")
             self.stop()
             return True
             
