8c8dcb15b41bdd4eb7f2344ea7dace236ca48c93
Sebastian Callh <sebca553@ixtab.edu.isy.liu.se>
2016-11-17 11:32:49 +0100

    ch-ch-ch-changes

--- pi/gyro_driver.py
+++ pi/gyro_driver.py
@@ -7,19 +7,22 @@ from driver import Driver
 gyro = Gyro()
 gyro.initialize()
 
-time_delta = datetime.now()
+previous_time = datetime.now()
 degrees_total = 0
 
 GYRO_LOWER_LIMIT = 10
 
 while(True):
     data = gyro.read_data()
+    
+    time_delta = (previous_time - datetime.now()).total_seconds()
+    delta_degrees =  data * time_delta
+    previous_time = datetime.now()
 
+    #Has to be run after previous time is set
     if abs(data) <= GYRO_LOWER_LIMIT:
         continue
-        
-    time_delta = time_delta - datetime.now()
-    delta_degrees =  data * time_delta.total_seconds()
+    
     degrees_total += delta_degrees
     print(degrees_total)
         
