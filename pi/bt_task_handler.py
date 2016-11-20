9420429c1f53595eed5551e5ff40ab6e8ec45189
Matildha <tildhanor@gmail.com>
2016-11-20 15:57:12 +0100

    prints

--- pi/bt_task_handler.py
+++ pi/bt_task_handler.py
@@ -26,8 +26,10 @@ def pop_incoming():
     to_server_queue = open("to_server.txt", "rb")
     try:
         task = pickle.load(to_server_queue)
+        print("task typ ar: ", type(task))
     except EOFError:
         task = None
+    print("task värde på id: ", task.cmd_id)
     return task
     
 # kallas från server
