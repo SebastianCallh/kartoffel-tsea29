31bcb24a8b8d1ec070e73a2ff2ac74528f305fd3
Sebastian Callh <sebca553@ixtab.edu.isy.liu.se>
2016-11-20 13:27:37 +0100

    pep8 lcd

--- pi/lcd.py
+++ pi/lcd.py
@@ -30,59 +30,61 @@ ENTRY_MODE_SET = [0, 0, 0, 0, 0, 1, 1, 0] #increment mode, entire shift off
 DISPLAY_ON = [0, 0, 0, 0, 1, 1, 0, 0]	
 
 class LCD:
-	def initialize(self):	
-		#Use board numbering
-		GPIO.setmode(GPIO.BOARD)
 
-		#Configure output pins
-		GPIO.setup(RS, GPIO.OUT)
-		GPIO.setup(E, GPIO.OUT)
+
+    def initialize(self):	
+        #Use board numbering
+        GPIO.setmode(GPIO.BOARD)
+
+        #Configure output pins
+        GPIO.setup(RS, GPIO.OUT)
+        GPIO.setup(E, GPIO.OUT)
         GPIO.setup(pins, GPIO.OUT)
 
 
-		#Power up sequence
-		GPIO.output(RS, 0)		    #Select instruction register
-		
-		sleep_ms(100)				#Make sure at least 30 ms has passed since power on
-		self.send(FUNCTION_SET)
-		sleep_us(80)				#Make super sure that 39 us has passed
-		self.send(DISPLAY_ONOFF_CONTROL)
-		sleep_us(80)				#Make super sure that 39 us has passed again
-		self.send(DISPLAY_CLEAR)
-		sleep_ms(4)				    #Make sure that 1.53 ms has passed
-		self.send(ENTRY_MODE_SET)
-		sleep_ms(10)
-		#End of power up sequence. Display is off
+        #Power up sequence
+        GPIO.output(RS, 0)		    #Select instruction register
+        
+        sleep_ms(100)				#Make sure at least 30 ms has passed since power on
+        self.send(FUNCTION_SET)
+        sleep_us(80)				#Make super sure that 39 us has passed
+        self.send(DISPLAY_ONOFF_CONTROL)
+        sleep_us(80)				#Make super sure that 39 us has passed again
+        self.send(DISPLAY_CLEAR)
+        sleep_ms(4)				    #Make sure that 1.53 ms has passed
+        self.send(ENTRY_MODE_SET)
+        sleep_ms(10)
+        #End of power up sequence. Display is off
 
-		#Turn on display
-		#self.send(DISPLAY_ON)
-		sleep_us(80)				#Wait a lot 
-
-
-
-	def send(self, data):
-		GPIO.output(E, 0) 		#Make sure E is initially low
-		GPIO.output(RS, 1)		#Select data register
-
-		#Put the data on the pins
-		for d, p in zip(data, pins):
-			GPIO.output(p, d)
-
-		#Write the data
-		GPIO.output(E, 1)
-		sleep_us(1)				#Larger than recommended wait
-		GPIO.output(E, 0)
-		sleep_us(1)				#Larger than recommended wait
+        #Turn on display
+        #self.send(DISPLAY_ON)
+        sleep_us(80)				#Wait a lot 
 
 
-	def cleanup(self):
-		GPIO.cleanup()
+    def send(self, data):
+        GPIO.output(E, 0) 		#Make sure E is initially low
+        GPIO.output(RS, 1)		#Select data register
+
+        #Put the data on the pins
+        for d, p in zip(data, pins):
+            GPIO.output(p, d)
+
+        #Write the data
+        GPIO.output(E, 1)
+        sleep_us(1)				#Larger than recommended wait
+        GPIO.output(E, 0)
+        sleep_us(1)				#Larger than recommended wait
+
+
+    def cleanup(self):
+        GPIO.cleanup()
 
 
 #Sleep in ms
 def sleep_ms(t):
-	sleep(t / 1000)
+    sleep(t / 1000)
 
+    
 #Sleep in us
 def sleep_us(t):
-	sleep(t / 10000)
+    sleep(t / 10000)
