e50222e3ebfba14106e30ba6913e3f303fcc58e6
Sebastian Callh <sebca553@ixtab.edu.isy.liu.se>
2016-11-20 15:05:25 +0100

    c example, removed åäö

--- pi/lcd.py
+++ pi/lcd.py
@@ -1,13 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-#Times in ns
-E_CYCLE = 500
-E_RISE_FALL = 20
-E_PULSE_WIDTH = 230
-
-DATA = [0 for x in range(0, 7)]
-
 '''
 Anslutningar till GPIO-portar. Alla portar ar i board-numbering
 GPIO 	29, 31, 32, 33, 35, 36, 37, 38
@@ -18,16 +11,25 @@ A		02
 K		06
 '''
 
+
+#Times in ns
+E_CYCLE = 500
+E_RISE_FALL = 20
+E_PULSE_WIDTH = 230
+
+DATA = [0 for x in range(0, 7)]
+
+
 #29 is MSB, 38 LSB
 PINS = [29, 31, 32, 33, 35, 36, 37, 38]
 RS = 22
 E = 40
 
 FUNCTION_SET = [0, 0, 1, 1, 0, 0, 0, 0]
-DISPLAY_ONOFF_CONTROL = [0, 0, 0, 0, 1, 1, 1, 1] #display, cursor, blink on 
+DISPLAY_ONOFF_CONTROL = [0, 0, 0, 0, 1, 0, 1, 1] #display, cursor, blink on 
 DISPLAY_CLEAR = [0, 0, 0, 0, 0, 0, 0, 1]
 ENTRY_MODE_SET = [0, 0, 0, 0, 0, 1, 1, 0] #increment mode, entire shift off
-DISPLAY_ON = [0, 0, 0, 0, 1, 1, 0, 0]	
+DISPLAY_ON = [0, 0, 0, 0, 1, 1, 1, 1]	
 RESET_CURSOR = [0, 0, 0, 0, 0, 0, 1]
 
 BIT_PATTERN = {
@@ -57,8 +59,6 @@ BIT_PATTERN = {
     'X': '01011000',
     'Y': '01011001',
     'Z': '01011010',
-    'Å': '11110100',
-    'Ö': '11100001',
     '0': '11101111',
     '1': '00110001',
     '2': '00110010',
@@ -85,10 +85,22 @@ class LCD:
         GPIO.setup(E, GPIO.OUT)
         GPIO.setup(PINS, GPIO.OUT)
 
-        
+
         #Power up sequence
         GPIO.output(RS, 0)		    #Select instruction register
         sleep_ms(100)				#Make sure at least 30 ms has passed since power on
+        
+        
+        self._send(FUNCTION_SET)
+        sleep_ms(10)
+        
+        self._send(FUNCTION_SET)
+        sleep_us(200)
+        
+        self._send(FUNCTION_SET)
+        sleep_ms(200)
+        
+        
         self._send(FUNCTION_SET)
         sleep_us(80)				#Make super sure that 39 us has passed
         self._send(DISPLAY_ONOFF_CONTROL)
@@ -100,7 +112,7 @@ class LCD:
         
         #End of power up sequence. Display is off
         #Turn on display
-        #self._send(DISPLAY_ON)
+        self._send(DISPLAY_ON)
 
 
     def clear(self):
