ced4e94e3b99b5f8cef89332a871744a228786c6
Matildha <tildhanor@gmail.com>
2016-11-20 14:58:21 +0100

    changed how files are handled in task_handler

--- pi/bt_task_handler.py
+++ pi/bt_task_handler.py
@@ -4,8 +4,8 @@ import pickle
 # NOTE: If multiple instances of bt_server_intermediary
 # are running simultaneously, the may erase each others
 # content
-to_server_queue = open("to_server.txt", "wb+")
-from_server_queue = open("from_server.txt", "wb+")
+to_server_queue = None
+from_server_queue = None
 
 
 class BT_task:
@@ -16,13 +16,22 @@ class BT_task:
     def __init__(self, cmd_id = 0, data = 0):
         self.cmd_id = cmd_id
         self.data = data
+        
+def open_medetiary_files():
+    global to_server_queue
+    global from_server_queue
+    to_server_queue = open("to_server.txt", "wb+")
+    from_server_queue = open("from_server.txt", "wb+")
+    
 
 # kallas från main
-def post_outgoing(bt_task):
+def post_outgoing(bt_task):  
+    global from_server_queue
     pickle.dump(bt_task, from_server_queue)
 
 # kallas från main
 def pop_incoming():
+    global to_server_queue
     try:
         task = pickle.load(to_server_queue)
     except EOFError:
@@ -31,10 +40,12 @@ def pop_incoming():
     
 # kallas från server
 def post_incoming(bt_task):
+    global to_server_queue
     pickle.dump(bt_task, to_server_queue)
 
 # kallas från server
 def pop_outgoing():
+    global from_server_queue
     try:
         task = pickle.load(from_server_queue)
     except EOFError:
