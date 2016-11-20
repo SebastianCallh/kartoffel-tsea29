dcc8d0ec1c5e877d59432eb25a291f079e08eec5
Matildha <tildhanor@gmail.com>
2016-11-20 17:27:27 +0100

    WE ARE IDIOTSSSS

--- pi/bt_server.py
+++ pi/bt_server.py
@@ -68,5 +68,5 @@ class BT_Server:
         if type(task) == bt_task_handler.BT_task and task.cmd_id != 0:
             print("update_outgoing: i if-sats")
             self.outgoing_data = str(task.cmd_id) + " " + str(task.data)  # TODO will change when json
-            has_new_outoing = True
+            has_new_outgoing = True
         return has_new_outgoing
