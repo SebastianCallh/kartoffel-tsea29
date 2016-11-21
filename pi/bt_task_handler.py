cf9a9e2f779a10fbb39856fca7d3f2ab904177b4
becczor <rebecca.lindblom@gmail.com>
2016-11-21 15:09:04 +0100

    fixed unresolved reference to task

--- pi/bt_task_handler.py
+++ pi/bt_task_handler.py
@@ -1,6 +1,7 @@
 import pickle
-#import psutil
+# import psutil
 import traceback, sys
+
 
 # Files in which to store BT_task objects in transition
 def peek_line(f):
@@ -9,38 +10,41 @@ def peek_line(f):
     f.seek(pos)
     return line
 
-class BT_task:
 
+class BT_task:
     # NOTE If the client always asks for certain data
     # it might not need to check it when it arrives?
 
-    def __init__(self, cmd_id = 0, data = 0):
+    def __init__(self, cmd_id=0, data=0):
         self.cmd_id = int(cmd_id)
         self.data = data
-  
+
+
 def clean_queue_files():
-    #Create files or erase previous content
-    answer_queue = open("bt_answers.txt","w")
-    #answer_queue.seek(0)
-    #answer_queue.truncate()
-    command_queue = open("bt_commands.txt","w")
-    #command_queue.seek(0)
-    #command_queue.truncate()
+    # Create files or erase previous content
+    answer_queue = open("bt_answers.txt", "w")
+    # answer_queue.seek(0)
+    # answer_queue.truncate()
+    command_queue = open("bt_commands.txt", "w")
+    # command_queue.seek(0)
+    # command_queue.truncate()
     answer_queue.close()
     command_queue.close()
 
+
 # kallas från main
-def post_outgoing(bt_task):  
+def post_outgoing(bt_task):
     answer_queue = open("bt_answers.txt", "ab")
     pickle.dump(bt_task, answer_queue)
-    #pickle.Pickler.clear_memo()
     answer_queue.close()
+
 
 # kallas från main
 def pop_incoming():
     command_queue = open("bt_commands.txt", "rb")
+    task = None
     try:
-        #psutil.phymem_usage()
+        # psutil.phymem_usage()
         print("peek coomad_queue: ", peek_line(command_queue))
         task = pickle.load(command_queue)
         print("Able to load, task-ID =", task.cmd_id)
@@ -50,35 +54,38 @@ def pop_incoming():
         target_command_queue = open("bt_commands.txt","wb")
         target_command_queue.writelines(tasks)
         target_command_queue.close()'''
-        #print("task typ ar: ", type(task))
+        # print("task typ ar: ", type(task))
     except EOFError:
         pass
     except MemoryError:
         print("pop_in: error = ", str(MemoryError))
-        #psutil.phymem_usage()
+        # psutil.phymem_usage()
         traceback.print_exc(file=sys.stdout)
         task = None
-    #print("task värde på id: ", task.cmd_id)
+    # print("task värde på id: ", task.cmd_id)
     command_queue.close()
     return task
-    
+
+
 # kallas från server
 def post_incoming(bt_task):
     command_queue = open("bt_commands.txt", "ab")
     print("task type in post_incoming ", type(bt_task))
     pickle.dump(bt_task, command_queue)
-    #pickle.Pickler.clear_memo(self=)
+    # pickle.Pickler.clear_memo(self=)
     command_queue.close()
+
 
 # kallas från server
 def pop_outgoing():
     answer_queue = open("bt_answers.txt", "rb")
+    task = None
     try:
-        #psutil.phymem_usage()
+        # psutil.phymem_usage()
         print("peek coomad_queue: ", peek_line(answer_queue))
         task = pickle.load(answer_queue)
         print("Able to load, task-ID =", task.cmd_id)
-        #psutil.phymem_usage()
+        # psutil.phymem_usage()
         # Remove first command in queue
         '''tasks = answer_queue.readlines()
         del tasks[0]
@@ -89,10 +96,8 @@ def pop_outgoing():
         pass
     except MemoryError:
         print("pop_in: error = ", str(MemoryError))
-        #psutil.phymem_usage()
+        # psutil.phymem_usage()
         traceback.print_exc(file=sys.stdout)
         task = None
     answer_queue.close()
     return task
-    
-
