84f75841496b90d0d3f0a360178964b3d39b4bb2
becczor <rebecca.lindblom@gmail.com>
2016-11-22 13:53:00 +0100

    added prints in main

--- pi/main.py
+++ pi/main.py
@@ -84,13 +84,13 @@ def main():
 
         # read_bt()
         bt_task = bt_task_handler.pop_incoming()
-        # print("type of task =", type(bt_task))
+        print("type of task =", type(bt_task))
         # print("task =", bt_task)
         if bt_task == None:
             # print("main: bt_task == None")
             pass
         elif int(bt_task.cmd_id) == protocol.REQUEST_PI_IP:
-            #print("main: bt_task.cmd_id = ", bt_task.cmd_id)
+            print("main: bt_task.cmd_id = ", bt_task.cmd_id)
             ip = bt_server_cmds.get_pi_ip()
             bt_task_handler.post_outgoing(bt_task_handler.BT_task(protocol.SEND_PI_IP, ip))
         elif int(bt_task.cmd_id) == 12:
