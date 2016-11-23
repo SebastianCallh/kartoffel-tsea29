cc8115d7f12c0ab0cce67547fceb12156b4d66fa
antondalgren <antda685@student.liu.se>
2016-11-23 16:10:04 +0100

    after turn we save latest IR to be all ir

--- pi/navigator.py
+++ pi/navigator.py
@@ -110,12 +110,6 @@ class turn(State):
         self.is_right_turn = is_right_turn
 
     def sensor_data_received(self, data, new_ir_left, new_ir_right):
-        data['old_ir_left'] = new_ir_left
-        data['old_ir_right'] = new_ir_right
-        data['ir_left'] = new_ir_left
-        data['ir_right'] = new_ir_right
-        data['new_ir_right'] = new_ir_right
-        data['new_ir_left'] = new_ir_left
         return #Do nothing. Only auto control uses it
 
     def run(self, data):
@@ -179,5 +173,9 @@ class Navigator:
 
         if curr_type is turn and next_type is not turn:
             EventBus.notify(CMD_TURN_FINISHED, self.state.is_right_turn)
+            self.data['old_ir_left'] = self.data['new_ir_left']
+            self.data['old_ir_right'] = self.data['new_ir_right']
+            self.data['ir_left'] = self.data['new_ir_left']
+            self.data['ir_right'] = self.data['new_ir_right']
 
         self.state = next_state
