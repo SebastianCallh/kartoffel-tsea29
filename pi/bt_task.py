a90fc27fcb3db522a8db45b3470b2a0066df109f
becczor <rebecca.lindblom@gmail.com>
2016-11-28 14:23:39 +0100

    Commented functions, renamed some varibles and removed unneccesary prints

--- pi/bt_task.py
+++ pi/bt_task.py
@@ -1,6 +1,8 @@
 class BT_task:
-    # NOTE If the client always asks for certain data
-    # it might not need to check it when it arrives?
+    """
+    Class for packing command ID and data when sending
+    commands and corresponding answers over Bluetooth.
+    """
 
     def __init__(self, cmd_id=0, data=0):
         self.cmd_id = int(cmd_id)
