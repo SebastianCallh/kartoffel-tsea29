2abc4c9d1de43a5f6d96eaccb7fe25ff4e87697f
Matilda Dahlström <matilda.dahlstrom@gmail.com>
2016-11-28 12:48:29 +0100

    Prints

--- pi/navigator.py
+++ pi/navigator.py
@@ -39,12 +39,11 @@ class auto_control(State):
         right_old_diff = abs(data['ir_right'] - data['old_ir_right'])
         right_new_diff = abs(data['new_ir_right'] - data['old_ir_right'])
 
-        if datetime.now() - data['last_updated_time'] > timedelta(milliseconds=UPDATE_FREQUENCY):
-            print("old_ir: " + str(data['old_ir_right']))
-            print("cur_ir: " + str(data['ir_right']))
-            print("new_ir: " + str(data['new_ir_right']))
+        print("old_ir: " + str(data['old_ir_right']))
+        print("cur_ir: " + str(data['ir_right']))
+        print("new_ir: " + str(data['new_ir_right']))
 
-            print("At right turn, old diff: " + str(right_old_diff) + ", new diff: " + str(right_new_diff))
+        print("At right turn, old diff: " + str(right_old_diff) + ", new diff: " + str(right_new_diff))
 
         return  right_old_diff >= Navigator.DISCONTINUITY_DIST and \
                 right_new_diff >= Navigator.DISCONTINUITY_DIST and \
@@ -167,7 +166,6 @@ class Navigator:
         self.data['new_ir_left'] = new_ir_left
         self.data['new_ir_left_back'] = new_ir_left_back_mm
         self.data['new_ir_right_back'] = new_ir_right_back_mm
-        self.data['last_updated_time'] = datetime.now()
 
         if self.data['old_ir_right'] == -1 and self.data['ir_right'] == -1:
             self.data['ir_right'] = self.data['new_ir_right']
