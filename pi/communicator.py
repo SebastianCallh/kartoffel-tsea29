df4743f1430991aca453d772365674463fe4df9c
Matildha <tildhanor@gmail.com>
2016-12-05 12:14:45 +0100

    ip to list

--- pi/communicator.py
+++ pi/communicator.py
@@ -16,30 +16,31 @@ class Communicator:
     "ir_left, ir_left_back, ir_right, ir_right_back, laser, gyro"
     '''
     def send_sensor_data(self):
-        outbound.bt_return_sensor_data(str(self.ir.get_ir_left()) + ', ' +
-                                       str(self.ir.get_ir_left_back()) + ', ' +
-                                       str(self.ir.get_ir_right()) + ', ' +
-                                       str(self.ir.get_ir_right_back()) + ', ' +
-                                       str(self.laser.get_data()) + ', ' +
-                                       str(self.gyro.get_data()))
+        outbound.bt_return_sensor_data(str([self.ir.get_ir_left()] +
+                                       [self.ir.get_ir_left_back()] +
+                                       [self.ir.get_ir_right()] +
+                                       [self.ir.get_ir_right_back()] +
+                                       [self.laser.get_data()] +
+                                       [self.gyro.get_data()]))
 
     '''
     Sends the sensor data as a string of integers on the format
     "left_speed, right_speed"
     '''
     def send_servo_data(self):
-        outbound.bt_return_servo_data(str(self.driver.get_left_speed()) + ', ' +
-                                      str(self.driver.get_right_speed()))
+        outbound.bt_return_servo_data(str([self.driver.get_left_speed()] +
+                                      [self.driver.get_right_speed()]))
 
     '''
     Sends the map data as a list with tuples of integers corresponding to corner coordinates on the format
     "[(X1, Y1), (X2, Y2), ... , (Xn, Yn)]"
     '''
     def send_map_data(self):
+        map_data = self.position.get_map_data()
         outbound.bt_return_map_data(str(self.position.get_map_data()))
 
     def send_ip(self):
-        outbound.bt_return_ip(get_ip())
+        outbound.bt_return_ip([get_ip()])
         print("send_ip, done")
 
         
