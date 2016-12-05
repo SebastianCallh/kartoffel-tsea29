33da097667a1b36bb3082cc4875521db9fe45e1e
antondalgren <antda685@student.liu.se>
2016-12-05 14:55:02 +0100

    Added calculations for mapdata and print for coordinates

--- pi/position.py
+++ pi/position.py
@@ -1,7 +1,7 @@
 from eventbus import EventBus
 from laser import Laser
 from protocol import CMD_TURN_STARTED, CMD_TURN_FINISHED
-from section import Section, NORTH
+from section import Section, NORTH, EAST, SOUTH, WEST
 from threading import Thread
 
 STATE_MEASURING = 0
@@ -14,6 +14,9 @@ class Position:
         self.state = STATE_MEASURING
         self.current_section = Section(NORTH)
         self.saved_sections = []
+        self.map_data = []
+        self.current_x = 0
+        self.current_y = 0
 
         EventBus.subscribe(CMD_TURN_STARTED, self.on_turning_started)
         EventBus.subscribe(CMD_TURN_FINISHED, self.on_turning_finished)
@@ -26,12 +29,14 @@ class Position:
     def save_current_section(self):
         self.current_section.finish()
 
+        self.saved_sections.append(self.current_section)
+        self.map_data.append(self.transform_map_data(self.current_section))
         print('---- SECTION SAVED ----')
         print('  direction: ' + str(self.current_section.direction))
         print('  distance: ' + str(self.current_section.block_distance))
+        print('  coordinates: ' + str(self.current_x) + ", " + str(self.current_y))
         print('-----------------------')
 
-        self.saved_sections.append(self.current_section)
 
     def begin_next_section(self, is_right_turn):
         if is_right_turn:
@@ -49,8 +54,25 @@ class Position:
         self.begin_next_section(is_right_turn)
         self.state = STATE_MEASURING
 
+    def transform_map_data(self, section):
+        if section.direction == NORTH:
+            self.current_y += section.block_distance
+        elif section.direction == EAST:
+            self.current_x += section.block_distance
+        elif section.direction == SOUTH:
+            self.current_y -= section.block_distance
+        elif section.direction == WEST:
+            self.current_x -= section.block_distance
+        return self.current_x, self.current_y
+
     '''
     Returns a list of tuples with coordinates of all the corners
     '''
     def get_map_data(self):
-        return [(10, 20), (30, 10)]
+        return [self.map_data]
+
+    '''
+    Returns the robots last know x,y coordinates
+    '''
+    def get_current_position(self):
+        return self.current_x, self.current_y
