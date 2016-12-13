6ba38f0f0a56f2f7286a25c411df400face01294
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-12-13 18:46:05 +0100

    Lower speed when approaching right turn

--- pi/autocontroller.py
+++ pi/autocontroller.py
@@ -9,12 +9,12 @@ last_diff = 0
 
 
 class AutoController:
+    DESIRED_DISTANCE = 120  # Desired distance to wall
+    STANDARD_SPEED = 40
+    MAX_REGULATION = 30
+
     def auto_control(self, ir_right_mm, ir_right_back_mm, reg_side):
         global use_derivate, time_last_regulation, old_error, integral, last_diff
-
-        DESIRED_DISTANCE = 120  # Desired distance to wall
-        STANDARD_SPEED = 40
-        MAX_REGULATION = 30
 
         Kp = float(0.2)
         Ka = float(0.3)
@@ -24,7 +24,7 @@ class AutoController:
         sensor_data_back = ir_right_back_mm
         dist_diff = (sensor_data_back - sensor_data_front)
 
-        regulation_error = DESIRED_DISTANCE - sensor_data_front + abs(dist_diff / 10)
+        regulation_error = self.DESIRED_DISTANCE - sensor_data_front + abs(dist_diff / 10)
 
 
         if (sensor_data_front == -1 or sensor_data_back == -1):
@@ -36,21 +36,27 @@ class AutoController:
         old_error = regulation_error
         last_diff = dist_diff
 
-        if (regulation > MAX_REGULATION):
-            regulation = MAX_REGULATION
-        elif (regulation < -MAX_REGULATION):
-            regulation = -MAX_REGULATION
+        if (regulation > self.MAX_REGULATION):
+            regulation = self.MAX_REGULATION
+        elif (regulation < -self.MAX_REGULATION):
+            regulation = -self.MAX_REGULATION
 
         if (regulation > -10):
-            speed_close_wall = STANDARD_SPEED + regulation
+            speed_close_wall = self.get_speed(ir_right_mm, ir_right_back_mm) + regulation
         else:
             speed_close_wall = 10
 
         if (regulation < 10):
-            speed_far_wall = STANDARD_SPEED - regulation
+            speed_far_wall = self.get_speed(ir_right_mm, ir_right_back_mm) - regulation
         else:
             speed_far_wall = 10
 
         time_last_regulation = time_now
         
         return int(speed_close_wall), int(speed_far_wall), regulation
+
+    def get_speed(self, ir_right_mm, ir_right_back_mm):
+        if ir_right_mm == -1 and ir_right_back_mm != -1:
+            return self.STANDARD_SPEED / 2
+        else:
+            return self.STANDARD_SPEED
