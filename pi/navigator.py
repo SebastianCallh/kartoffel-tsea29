d43f126941dff3d6411fff30b32fdfd6d5c29426
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-12-13 19:43:09 +0100

    Decrease left turn start distance

--- pi/navigator.py
+++ pi/navigator.py
@@ -86,7 +86,7 @@ class Navigator:
     AUTONOMOUS = 1
     
     DISCONTINUITY_DIST = 25.0  # mm
-    FACING_WALL_DIST = 300  # mm
+    FACING_WALL_DIST = 250  # mm
 
     right_turn_enabled = True
     force_left_turn = False
